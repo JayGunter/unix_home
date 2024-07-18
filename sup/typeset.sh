aa0b3a37a7e8236bc0f22225bbac89671daf89d7
Jay Gunter <jay@gunter.io>
2024-07-17 18:48:40 -0700

    wip

--- sup/typeset.sh
+++ sup/typeset.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-out=$1.html
+out=${1%.txt}.html
 
 cat > $out << END
 <html>
@@ -8,13 +8,24 @@ cat > $out << END
 END
 
 sed < $1 >> $out ' 
+# title
   s@\#\#\# \(.*\)$@<h3>\1</h3>@; 
+# paragraph
   s@^  @<br/>\&emsp;@;
-  s@\*\(.*\)\*@<i>\1</i>@g; s@^$@<p/>@;
+# italicize stuff between asterisks
+  s@\*\([^*]*\)\*@<i>\1</i>@g; s@^$@<p/>@;
+# n-dash after mid-line sentence ending
   s@\(["?.]\)  @\1\&ensp;@g;
+# n-dash after end-line sentence ending with trailing spaces
   s@\(["?.]\) *$@\1@;
+# n-dash after end-line sentence ending
   s@\(["?.]\)$@\1\&ensp;@g;  
-  s@\.)@.)\&ensp;@g
+# n-dash after sentence ending in close-paren
+  s@\.)@.)\&ensp;@g;
+# remove // comments in text
+  /^\/\//d;
+# long hyphen
+  s@--@\&mdash;@g;
   '
 
 cat >> $out << END
